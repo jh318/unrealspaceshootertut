907027175fba2d53fc1f807ebb46c13377ad8a94
jh318 <jhawley318@gmail.com>
2016-10-16 20:46:44 -0500

    commit completed project tut

---
+++ SpaceShooter/Source/SpaceShooter/EnemyController.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "EnemyController.generated.h"
+
+UCLASS()
+class SPACESHOOTER_API AEnemyController : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemyController();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	UPROPERTY(EditAnywhere)
+		UShapeComponent* RootBox;
+
+	UPROPERTY(EditAnywhere)
+		float Speed = -200.0f;
+	
+};
